412df80da546ba1585a203e0afacc67b03ad67eb
fadilnatakusumah <fadil.ntksmh@gmail.com>
2021-03-27 18:21:50 +0700

    add error image asset

--- lib/pages/article_detail_page.dart
+++ lib/pages/article_detail_page.dart
@@ -69,6 +69,9 @@ class _ArticleDetailPageState extends State<ArticleDetailPage> {
           _isLoading = false;
         });
       }
+    }).catchError((error) {
+      print("error $error");
+      Navigator.of(context).pop();
     });
   }
 
